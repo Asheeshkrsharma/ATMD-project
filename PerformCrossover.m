a6a4c72eef080de3d4c4098931c84026bf014c22
Asheeshkrsharma <asheeshkrsharma@me.com>
2017-05-05 15:14:59 -0700

    Delete PerformCrossover.m

--- PerformCrossover.m
+++
@@ -1,5 +0,0 @@
-function [ newChromosome1 , newChromosome2 ] = PerformCrossover(c1,c2)
-    pos1=randi(length(c1)-1) ;
-    newChromosome1 = [ c1(1:pos1) c2(pos1+1: length(c1))] ;
-    newChromosome2 = [ c2(1:pos1) c1(pos1+1: length(c1))] ;
-end
